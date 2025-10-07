66bb7c26ab9296ff931d5298f94e223d862f2666
Pavel <p.a.stepanovich@gmail.com>
2025-10-07 13:37:36 +1000

    Привел в порядок оформление кода

--- PLATON_tbot.py
+++ PLATON_tbot.py
@@ -21,23 +21,17 @@ bot = telebot.TeleBot(os.environ.get("TELEGRAM_BOT_TOKEN"))
 
 @bot.message_handler(func=lambda _: True)
 def handler_message(message):
-
-
-config = {"configurable": {"thread_id": message.from_user.id}}
-
-query = message.text
-
-input_messages = [HumanMessage(query)]
-output = app.invoke({"messages": input_messages}, config)
-bot_anwser = output["messages"][-1].content
-bot.send_message(message.chat.id, bot_anwser)
+    config = {"configurable": {"thread_id": message.from_user.id}}
+    query = message.text
+    input_messages = [HumanMessage(query)]
+    output = app.invoke({"messages": input_messages}, config)
+    bot_anwser = output["messages"][-1].content
+    bot.send_message(message.chat.id, bot_anwser)
 
 
 # Функция main
 def main():
-
-
-bot.polling(none_stop=True)
+    bot.polling(none_stop=True)
 
 # Запускаем программу
 if __name__ == '__main__':
@@ -47,22 +41,20 @@ if __name__ == '__main__':
         model="GigaChat-Max",
         verify_ssl_certs=False,
     )
-# Инициализируйте граф
-workflow = StateGraph(state_schema=MessagesState)
+    # Инициализируйте граф
+    workflow = StateGraph(state_schema=MessagesState)
 
 
-def call_model(state: MessagesState):
-    response = model.invoke(state["messages"])
+    def call_model(state: MessagesState):
+        response = model.invoke(state["messages"])
+        return {"messages": response}
 
+    # Задайте вершину графа
+    workflow.add_edge(START, "model")
+    workflow.add_node("model", call_model)
 
-return {"messages": response}
+    # Добавьте память
+    memory = MemorySaver()
+    app = workflow.compile(checkpointer=memory)
 
-# Задайте вершину графа
-workflow.add_edge(START, "model")
-workflow.add_node("model", call_model)
-
-# Добавьте память
-memory = MemorySaver()
-app = workflow.compile(checkpointer=memory)
-
-main()
+    main()
